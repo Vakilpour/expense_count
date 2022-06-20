78e6b08acd096e67d539c24018fe9a4b152db364
Arsess Vakilpour - dotzilla GmbH A.vakilpour Co. KG <a.vakilpour@dotzilla.de>
2022-06-20 14:44:41 +0200

    Add multiple Extence component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import ExpenseItemGenerator from './components/ExpenseItemGenerator'
+import Expenses from "./components/Expenses"
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const expense = [
-    { date: new Date, amount: '599 pln' },
-    { date: new Date, amount: '699 pln' },
-    { date: new Date, amount: '799 pln' }
-  ]
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItemGenerator
-      data={expense}/>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
